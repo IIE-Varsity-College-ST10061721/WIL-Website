2f3059be8200164ee206eb5ebb6b48ed64088982
Aphiwe Buthelezi <st10084765@vcconnect.edu.za>
2024-08-19 21:07:55 +0200

    Commit 1

---
+++ FeedTheFurballsMVC/FeedTheFurballsMVC/Controllers/SupportersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FeedTheFurballsMVC.Controllers
+{
+    public class SupportersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
