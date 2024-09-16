61d9828cc8361233d46737793d5907e8cee1db88
Aphiwe Buthelezi <st10084765@vcconnect.edu.za>
2024-09-16 19:20:23 +0200

    Next Update

---
+++ FeedTheFurballsMVC/FeedTheFurballsMVC/Models/DonationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FeedTheFurballsMVC.Models
+{
+    public class DonationViewModel
+    {
+        public decimal Goal { get; set; }
+        public decimal TotalDonations { get; set; }
+        public decimal Remaining => Goal - TotalDonations;
+    }
+}
