d102be97672554bc487231a61a151103a5531007
Dillon Wernich <st10061721@vcconnect.edu.za>
2024-09-17 13:43:03 +0200

    Commit

---
+++ FeedTheFurballsMVC/FeedTheFurballsMVC/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FeedTheFurballsMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            // Replace with actual authentication logic
+            if (username == "admin" && password == "password")
+            {
+                // Redirect to admin dashboard after successful login
+                return RedirectToAction("Index", "Admin");
+            }
+
+            // Add an error message and return to the login view
+            ViewBag.Error = "Invalid credentials.";
+            return View();
+        }
+    }
+
+}
---
+++ FeedTheFurballsMVC/FeedTheFurballsMVC/Controllers/DonationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FeedTheFurballsMVC.Controllers
+{
+    public class DonationController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new Models.DonationViewModel
+            {
+                Goal = 25000,
+                TotalDonations = 10000
+            };
+            return View(model);
+        }
+    }
+}
